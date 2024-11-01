3558775de99129e2718a9b7a01afada7d27d16e5
Miłosz Smółka <milosz.smolka@gmail.com>
2024-11-01 13:35:19 +0100

    Better tables

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
-	"log"
 	"strconv"
 	"strings"
 
@@ -22,27 +22,106 @@ func main() {
 	flag.Parse()
 	sizes := strings.Split(*messageSizes, ",")
 
+	var pubResults []pkg.Results
 	var subResults []pkg.Results
 
 	for _, size := range sizes {
 		s, err := strconv.Atoi(size)
 		if err != nil {
-			log.Fatal(err)
+			panic(err)
 		}
 
 		fmt.Printf("Starting benchmark for PubSub %s (%d messages, %d bytes each)\n",
 			*pubsubFlag, *messagesCount, s)
 
-		_, subRes, err := pkg.RunBenchmark(*pubsubFlag, *messagesCount, s)
+		pubRes, subRes, err := pkg.RunBenchmark(*pubsubFlag, *messagesCount, s)
 		if err != nil {
-			log.Fatal(err)
+			panic(err)
 		}
 
+		pubResults = append(pubResults, pubRes)
 		subResults = append(subResults, subRes)
 	}
 
-	fmt.Printf("messages\tmessage size\trate (messages/s)\tthroughput (b/s)\n")
-	for _, r := range subResults {
-		fmt.Printf("%d\t%d\t%f\t%f\n", r.Count, r.MessageSize, r.MeanRate, r.MeanThroughput)
+	fmt.Printf("\n\n")
+	fmt.Println(generateTable(pubResults, subResults))
+}
+
+func padRight(str string, length int) string {
+	if len(str) >= length {
+		return str
 	}
+	return str + strings.Repeat(" ", length-len(str))
+}
+
+func generateTable(pubResults, subResults []pkg.Results) string {
+	headers := []string{
+		"Message size (bytes)",
+		"Publish (messages / s)",
+		"Subscribe (messages / s)",
+	}
+
+	// Calculate max width for each column based on headers and data
+	colWidths := make([]int, len(headers))
+	for i, header := range headers {
+		colWidths[i] = len(header)
+	}
+
+	// Check data widths for each column including thousand separators
+	for i := 0; i < len(pubResults); i++ {
+		// Column 1: Message size
+		msgSizeWidth := len(fmt.Sprintf("%d", pubResults[i].MessageSize))
+		if msgSizeWidth > colWidths[0] {
+			colWidths[0] = msgSizeWidth
+		}
+
+		// Column 2: Publish rate
+		pubRateWidth := len(fmt.Sprintf("%d,%03d", int(pubResults[i].MeanRate)/1000, int(pubResults[i].MeanRate)%1000))
+		if pubRateWidth > colWidths[1] {
+			colWidths[1] = pubRateWidth
+		}
+
+		// Column 3: Subscribe rate
+		subRateWidth := len(fmt.Sprintf("%d,%03d", int(subResults[i].MeanRate)/1000, int(subResults[i].MeanRate)%1000))
+		if subRateWidth > colWidths[2] {
+			colWidths[2] = subRateWidth
+		}
+	}
+
+	var buf bytes.Buffer
+
+	// Write header
+	buf.WriteString("|")
+	for i, header := range headers {
+		buf.WriteString(" " + padRight(header, colWidths[i]) + " |")
+	}
+	buf.WriteString("\n")
+
+	// Write separator
+	buf.WriteString("|")
+	for _, width := range colWidths {
+		buf.WriteString("-" + strings.Repeat("-", width) + "-|")
+	}
+	buf.WriteString("\n")
+
+	// Write data rows
+	for i := 0; i < len(pubResults); i++ {
+		buf.WriteString("|")
+
+		// Message size (left-aligned)
+		msgSize := fmt.Sprintf("%d", pubResults[i].MessageSize)
+		buf.WriteString(" " + padRight(msgSize, colWidths[0]) + " |")
+
+		// Publish rate (left-aligned) with thousand separator
+		pubRate := fmt.Sprintf("%d,%03d", int(pubResults[i].MeanRate)/1000, int(pubResults[i].MeanRate)%1000)
+		buf.WriteString(" " + padRight(pubRate, colWidths[1]) + " |")
+
+		// Subscribe rate (left-aligned) with thousand separator
+		subRate := fmt.Sprintf("%d,%03d", int(subResults[i].MeanRate)/1000, int(subResults[i].MeanRate)%1000)
+		buf.WriteString(" " + padRight(subRate, colWidths[2]) + " |")
+
+		buf.WriteString("\n")
+	}
+
+	return buf.String()
 }
