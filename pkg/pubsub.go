eadbba2e42a544210ca6b9476fb85cb031596ad4
Miłosz Smółka <milosz.smolka@gmail.com>
2024-11-01 14:08:41 +0100

    Update psql

--- pkg/pubsub.go
+++ pkg/pubsub.go
@@ -406,11 +406,13 @@ type PostgreSQLSchema struct {
 func (p PostgreSQLSchema) SchemaInitializingQueries(topic string) []sql.Query {
 	createMessagesTable := strings.Join([]string{
 		`CREATE TABLE IF NOT EXISTS ` + p.MessagesTable(topic) + ` (`,
-		`"offset" SERIAL PRIMARY KEY,`,
+		`"offset" BIGSERIAL,`,
 		`"uuid" UUID NOT NULL,`,
 		`"created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,`,
 		`"payload" BYTEA DEFAULT NULL,`,
 		`"metadata" JSON DEFAULT NULL`,
+		`"transaction_id" xid8 NOT NULL,`,
+		`"PRIMARY KEY ("transaction_id", "offset")`,
 		`);`,
 	}, "\n")
 
